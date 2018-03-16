9729ec8991b5cbb38b72e917488e27b36b6d331b
aqent <ryan-hop@hotmail.com>
2018-03-16 13:33:05 -0500

    Initial commit. Also, the basic layout without code for the quiz app.

---
+++ app/src/main/java/com/example/ryan/thepowerofthepresident/WhatCanHeSheDo.java
@@ -0,0 +1,13 @@
+package com.example.ryan.thepowerofthepresident;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WhatCanHeSheDo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_what_can_he_she_do);
+    }
+}
